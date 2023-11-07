f2be3fe36622762351b0d918c9ab32ce272a954e
keys19 <kts5726@nyu.edu>
2023-11-07 16:35:16 +0400

    hsc page

---
+++ script.js
@@ -0,0 +1,64 @@
+window.onload = function () { 
+	let slides = 
+		document.getElementsByClassName('carousel-item'); 
+
+	function addActive(slide) { 
+		slide.classList.add('active'); 
+	} 
+
+	function removeActive(slide) { 
+		slide.classList.remove('active'); 
+	} 
+
+	addActive(slides[0]); 
+	setInterval(function () { 
+		for (let i = 0; i < slides.length; i++) { 
+			if (i + 1 == slides.length) { 
+				addActive(slides[0]); 
+				setTimeout(removeActive, 350, slides[i]); 
+				break; 
+			} 
+			if (slides[i].classList.contains('active')) { 
+				setTimeout(removeActive, 350, slides[i]); 
+				addActive(slides[i + 1]); 
+				break; 
+			} 
+		} 
+	}, 1500); 
+};
+
+
+// var gallery = document.querySelector('#gallery');
+// var getVal = function (elem, style) { return parseInt(window.getComputedStyle(elem).getPropertyValue(style)); };
+// var getHeight = function (item) { return item.querySelector('.content').getBoundingClientRect().height; };
+// var resizeAll = function () {
+//     var altura = getVal(gallery, 'grid-auto-rows');
+//     var gap = getVal(gallery, 'grid-row-gap');
+//     gallery.querySelectorAll('.gallery-item').forEach(function (item) {
+//         var el = item;
+//         el.style.gridRowEnd = "span " + Math.ceil((getHeight(item) + gap) / (altura + gap));
+//     });
+// };
+// gallery.querySelectorAll('img').forEach(function (item) {
+//     item.classList.add('byebye');
+//     if (item.complete) {
+//         console.log(item.src);
+//     }
+//     else {
+//         item.addEventListener('load', function () {
+//             var altura = getVal(gallery, 'grid-auto-rows');
+//             var gap = getVal(gallery, 'grid-row-gap');
+//             var gitem = item.parentElement.parentElement;
+//             gitem.style.gridRowEnd = "span " + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
+//             item.classList.remove('byebye');
+//         });
+//     }
+// });
+// window.addEventListener('resize', resizeAll);
+// gallery.querySelectorAll('.gallery-item').forEach(function (item) {
+//     item.addEventListener('click', function () {        
+//         item.classList.toggle('full');        
+//     });
+// });
+
+
